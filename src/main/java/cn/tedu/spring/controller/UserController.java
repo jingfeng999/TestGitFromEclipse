1a9de891ed2eb3a6261df4bbb07c4dd0d051b681
eclipse_user <eclipse_user@qq.com>
2020-03-18 20:26:11 +0800

    2 commit UserController

--- src/main/java/cn/tedu/spring/controller/UserController.java
+++ src/main/java/cn/tedu/spring/controller/UserController.java
@@ -11,7 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("users")
 public class UserController {
-	
+	String url=null;
+	/**
+	 * 用户注册
+	 * @param username 用户名
+	 * @param password 用户密码
+	 */
 	@RequestMapping("reg")
 	public void reg(String username, String password) {
 		return ;
